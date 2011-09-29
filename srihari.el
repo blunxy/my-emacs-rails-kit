84c7c838b79be70b0726fa5ec8800f9b3012a668
Srihari <srih4ri@gmail.com>
2011-09-29 22:45:39 +0530

    Set line numbers for ruby files
    Following http://avdi.org/devblog/2011/09/29/emacs-reboot-11-line-numbers

--- srihari.el
+++ srihari.el
@@ -1,2 +1,9 @@
- (global-set-key (kbd "M-.") 'hippie-expand)
- (global-set-key (kbd "<f6>") 'indent-buffer)
+(global-set-key (kbd "M-.") 'hippie-expand)
+(global-set-key (kbd "<f6>") 'indent-buffer)
+;;http://avdi.org/devblog/2011/09/29/emacs-reboot-11-line-numbers
+;;note-to-self Intention was to set linum mode when a some modes(ruby now) were loaded
+(add-hook 'srihari-code-modes-hook
+          (lambda () (linum-mode 1)))
+(add-hook 'ruby-mode-hook
+          (lambda () (run-hooks 'srihari-code-modes-hook)))
+;;done hooking to rubymode
