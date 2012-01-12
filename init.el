02e4a5141073e044d89b642278ac45c305e061ea
raykin <raykincoldxiao@gmail.com>
2012-01-12 10:03:48 +0800

    update emacs setting

--- init.el
+++ init.el
@@ -5,6 +5,7 @@
 (require 'recentf)
 
 (defvar root-dir "~/.emacs.d/")
+
 (defun load-lib (name)
   (load (concat root-dir name ".el")))
 (defun load-lib-dir (path)
@@ -27,7 +28,14 @@
 
 (load custom-file 'noerror)
 
+
 ;; Load user config and system config
+
+(add-to-list 'package-archives
+             '("marmalade" . "http://marmalade-repo.org/packages/") t)
+(package-initialize)
+
+
 (setq system-specific-config (concat root-dir system-name ".el")
       user-specific-config (concat root-dir user-login-name ".el"))
 
@@ -38,6 +46,7 @@
 
 ;; following add by myself
 (add-to-list 'load-path (expand-file-name "~/.emacs.d/rails-minor-mode"))
+
 (require 'rails)
 
 (add-hook 'before-save-hook 'delete-trailing-whitespace)
@@ -49,8 +58,8 @@
 ;;(set-default-font "Courier New-14")
 (set-face-attribute 'default nil :height 130)
 (global-set-key   [f2]   'ruby-mode)
-(global-set-key   [f5]   'goto-line)
-(global-set-key   [f6]   'revert-buffer)
+;; (global-set-key [f5] 'revert-buffer) ;; has defined in emacs-gui.el
+(global-set-key   [f6]   'goto-line)
 (global-set-key   [f7]   'rename-buffer)
 (global-set-key   [f8]   'search-backward-regexp)
 (global-set-key   [f9]   'search-forward-regexp)
@@ -69,6 +78,8 @@
 ;; (color-theme-calm-forest)
 ;;(require 'color-theme-solarized)
 (color-theme-solarized-dark)
+;;(load-file "~/.emacs.d/elpa/color-theme-railscasts-0.0.2/color-theme-railscasts.el")
+;;(color-theme-railscasts)
 ;; (color-theme-solarized-light)
 
 ;;(tool-bar-mode -1) ;; hide toolbar
@@ -80,16 +91,18 @@
 ;; used.
 ;; This version uses lwarn instead of message-box in the original version.
 ;; lwarn will open another window, and display the warning in there.
-(defun flymake-display-warning (warning)
-  "Display a warning to the user, using lwarn"
-  (lwarn 'flymake :warning warning))
-;; Using lwarn might be kind of annoying on its own, popping up windows and
-;; what not. If you prefer to recieve the warnings in the mini-buffer, use:
-(defun flymake-display-warning (warning)
-  "Display a warning to the user, using lwarn"
-  (message warning))
+;; (defun flymake-display-warning (warning)
+;;   "Display a warning to the user, using lwarn"
+;;   (lwarn 'flymake :warning warning))
+;; ;; Using lwarn might be kind of annoying on its own, popping up windows and
+;; ;; what not. If you prefer to recieve the warnings in the mini-buffer, use:
+;; (defun flymake-display-warning (warning)
+;;   "Display a warning to the user, using lwarn"
+;;   (message warning))
+
 (add-to-list 'auto-mode-alist '("\\.scss\\'" . css-mode))
 (add-to-list 'auto-mode-alist '("\\.js.erb\\'" . espresso-mode))
+
 (setq-default indent-tabs-mode nil)
 (setq-default tab-width 4)
 (setq indent-line-function 'insert-tab)
@@ -114,6 +127,96 @@
 
 ;;To startup quickly
 ;; (require 'ecb-autoloads)
+
+;; add ruby flymake check
+;;;;;;;;;;;;;;;;;;;; remove this after install flymake-ruby
+
+
+
+;;; flymake-ruby.el --- A flymake handler for ruby-mode files
+;;
+;;; Author: Steve Purcell <steve@sanityinc.com>
+;;; URL: https://github.com/purcell/flymake-ruby
+;;; Version: 0.4
+;;;
+;;; Commentary:
+;; Usage:
+;;   (require 'flymake-ruby)
+;;   (add-hook 'ruby-mode-hook 'flymake-ruby-load)
+;; (require 'flymake)
+
+;; ;;; Code:
+
+;; (defconst flymake-ruby-err-line-patterns '(("^\\(.*\\):\\([0-9]+\\): \\(.*\\)$" 1 2 nil 3)))
+
+;; (defvar flymake-ruby-executable "ruby"
+;;   "The ruby executable to use for syntax checking.")
+
+;; (defun flymake-ruby--create-temp-in-system-tempdir (file-name prefix)
+;;   "Return a temporary file name into which flymake can save.
+
+;; This is tidier than `flymake-create-temp-inplace', and therefore
+;; preferable when the checking doesn't depend on the file's exact
+;; location."
+;;   (make-temp-file (or prefix "flymake-ruby") nil ".rb"))
+
+;; ;; Invoke ruby with '-c' to get syntax checking
+;; (defun flymake-ruby-init ()
+;;   "Construct a command that flymake can use to check ruby source."
+;;   (list flymake-ruby-executable
+;;         (list "-c" (flymake-init-create-temp-buffer-copy
+;;                     'flymake-ruby--create-temp-in-system-tempdir))))
+
+;; ;;;###autoload
+;; (defun flymake-ruby-load ()
+;;   "Configure flymake mode to check the current buffer's ruby syntax.
+
+;; This function is designed to be called in `ruby-mode-hook'; it
+;; does not alter flymake's global configuration, so function
+;; `flymake-mode' alone will not suffice."
+;;   (interactive)
+;;   (set (make-local-variable 'flymake-allowed-file-name-masks) '(("." flymake-ruby-init)))
+;;   (set (make-local-variable 'flymake-err-line-patterns) flymake-ruby-err-line-patterns)
+;;   (if (executable-find flymake-ruby-executable)
+;;       (flymake-mode t)
+;;     (message "Not enabling flymake: '%' command not found" flymake-ruby-executable)))
+
+
+;; (provide 'flymake-ruby)
+;; ;;; flymake-ruby.el ends here
+;; (require 'flymake-ruby) (add-hook 'ruby-mode-hook 'flymake-ruby-load)
+
+
+;; add erb flymake check
+;; not good, cause it will add new files end with flymake.erb
+;;   with this command to delete such files : find . -type f -name '*flymake*' -exec rm '{}' \;
+
+;; (defun flymake-erb-init ()
+;;   (let* ((check-buffer (current-buffer))
+;;          (temp-file (flymake-create-temp-inplace (buffer-file-name) "flymake"))
+;;          (local-file (file-relative-name
+;;                       temp-file
+;;                       (file-name-directory buffer-file-name))))
+;;     (save-excursion
+;;       (save-restriction
+;;         (widen)
+;;         (with-temp-file temp-file
+;;           (let ((temp-buffer (current-buffer)))
+;;             (set-buffer check-buffer)
+;;             (call-process-region (point-min) (point-max) "erb" nil temp-buffer nil "-x"))))
+;;       (setq flymake-temp-source-file-name temp-file)
+;;       (list "ruby" (list "-c" local-file)))))
+
+;; (eval-after-load "flymake"
+;;   '(progn
+;;      (push '("\\.\\(erb\\|rhtml\\)$" flymake-erb-init) flymake-allowed-file-name-masks)
+;;      (push '("^\\(.*\\):\\([0-9]+\\): \\(.*\\)$" 1 2 nil 3) flymake-err-line-patterns)))
+
+;; (defun turn-on-flymake-for-erb-files ()
+;;   (when (string-match "\.\\(erb\\|rhtml\\)$" (buffer-file-name))
+;;     (flymake-mode-on)))
+;; (add-hook 'find-file-hook 'turn-on-flymake-for-erb-files)
+
 
 ;; init.el end
 
